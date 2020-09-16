0c73e0b77c3021b97cf6e91726e6887558624d92
nikatheengineer <npest17@freeuni.edu.ge>
2020-09-16 14:47:54 +0400

    Add test route for newbies

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,14 @@
   import { DeviceDetectorService } from "./services/deviceDetector.service";
   import * as Config from "./config";
 
+  import TestRoute from "./routes/Test.route.svelte"
+
   // Used for SSR. A falsy value is ignored by the Router.
   export let url = "";
 </script>
 
 <Router url="{url}">
-    
+    <Route path="test" component={TestRoute}></Route>
 </Router>
 
 Hello world, I wanna be a zoo shop manager one day :)
