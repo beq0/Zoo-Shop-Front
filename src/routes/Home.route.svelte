6a9d000e4a35d12d0f058d67d394ae9adad31935
Beka Gugulashvili <bgugu17@freeuni.edu.ge>
2020-10-31 18:26:14 +0400

    added results side to home page

--- src/routes/Home.route.svelte
+++ src/routes/Home.route.svelte
@@ -20,11 +20,18 @@
 
 <style>
 
+    .all-parts {
+        display: flex;
+        flex-direction: left;
+        justify-content: space-around;
+    }
+    
     .toolbar {
         display: flex;
         flex-direction: column;
         justify-content: space-between;
         height: 500px;
+        margin-top: 70px;
     }
 
     .filter-header {
@@ -65,43 +72,149 @@
         border-color: #3cb5cf;
     }
 
+
+    .vertical-line {
+        border-left: 12px solid #4adfff;
+        border-radius: 20px;
+        height: 620px;
+        margin-top: 30px;
+    }
+    
+    .results {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 500px;   
+        width: 700px;  
+        margin-top: 50px;   
+    }
+
+    .result-pair {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 70px;
+        text-align: center;
+    }
+    
+    .result-header {
+        font-size: 20px;
+        font-style: italic;
+        font-family: inherit;
+    }
+
+    .result-result {
+        font-size: 21px;
+        font-weight: bold;
+        font-family: inherit;
+    }
+
+    .result-count {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+    .btn-details {
+        font-size: 14px;
+        height: 35px;
+        background-color:#006c84;
+        border-color:#006c84;
+    }
+
+    .result-winning {
+        color: #00c400;
+    }
+
+    .result-losing {
+        color: #ff2e2e;
+    }
+
 </style>
 
-
-<div class="toolbar vertical-center">
-    <div class="toolbar-item">
-        <div class="filter-pair">
-            <div class="filter-header">სახელი:&emsp;</div>
-            <input type="text" class="form-control" bind:value={filterName}>
-        </div>
-    </div>
-
-    <div class="toolbar-item">
-        <div class="filter-pair">
-            <div class="filter-header">ტიპი:&emsp;</div>
-            <input type="text" class="form-control" bind:value={filterType}>
-        </div>
-    </div>
-
-    <div class="toolbar-item">
-        <div class="filter-pair">
-            <div class="filter-header">საწყისი&nbsp;თარიღი:&emsp;</div>
-            <input type="date" class="form-control date-filters" bind:value={filterStartDate}>
-        </div>
-    </div>
-
-    <div class="toolbar-item">
-        <div class="filter-pair">
-            <div class="filter-header">საბოლოო&nbsp;თარიღი:&emsp;</div>
-            <input type="date" class="form-control date-filters" bind:value={filterEndDate}>
-        </div>
-    </div>
-
-    <div class="filter-buttons">
-        <!-- svelte-ignore a11y-missing-attribute -->
-            <button type="button" class="btn btn-primary">ძებნა</button>
+<div class="all-parts">
+    <div>
+        <div class="toolbar">
+            <div class="toolbar-item">
+                <div class="filter-pair">
+                    <div class="filter-header">სახელი:&emsp;</div>
+                    <input type="text" class="form-control" bind:value={filterName}>
+                </div>
+            </div>
         
-        <!-- svelte-ignore a11y-missing-attribute -->
-            <button type="button" class="btn btn-primary">ფილტრის გასუფთავება</button>
+            <div class="toolbar-item">
+                <div class="filter-pair">
+                    <div class="filter-header">ტიპი:&emsp;</div>
+                    <input type="text" class="form-control" bind:value={filterType}>
+                </div>
+            </div>
+        
+            <div class="toolbar-item">
+                <div class="filter-pair">
+                    <div class="filter-header">საწყისი&nbsp;თარიღი:&emsp;</div>
+                    <input type="date" class="form-control date-filters" bind:value={filterStartDate}>
+                </div>
+            </div>
+        
+            <div class="toolbar-item">
+                <div class="filter-pair">
+                    <div class="filter-header">საბოლოო&nbsp;თარიღი:&emsp;</div>
+                    <input type="date" class="form-control date-filters" bind:value={filterEndDate}>
+                </div>
+            </div>
+        
+            <div class="filter-buttons">
+                <!-- svelte-ignore a11y-missing-attribute -->
+                    <button type="button" class="btn btn-primary">ძებნა</button>
+                
+                <!-- svelte-ignore a11y-missing-attribute -->
+                    <button type="button" class="btn btn-primary">ფილტრის გასუფთავება</button>
+            </div>
+        </div>
+    </div>
+    
+    
+    <div class="vertical-line"></div>
+
+    
+    <div class="results">
+        <div class="result-pair">
+            <div class="result-header">
+                გაყიდული პროდუქტების რაოდენობა:
+            </div>
+            <div class="result-result result-count">
+                <div>4091</div>
+                <div style="width:20px;"></div>
+                <button type="button" class="btn btn-primary btn-details">დეტალურად ნახვა</button>
+            </div>
+        </div>
+        
+        <div class="result-pair">
+            <div class="result-header">
+                გაყიდვის ფასი:
+            </div>
+            <div class="result-result result-winning">
+                5421
+            </div>
+        </div>
+        
+        <div class="result-pair">
+            <div class="result-header">
+                ყიდვის ფასი:
+            </div>
+            <div class="result-result result-losing">
+                521
+            </div>
+        </div>
+
+        <div class="result-pair">
+            <div class="result-header">
+                მოგება:
+            </div>
+            <div class="result-result result-winning">
+                418
+            </div>
+        </div>
+        
     </div>
 </div>
