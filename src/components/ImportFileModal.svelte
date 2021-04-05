28b786c331b814fd0bd66327ba8862f2ab9715e3
nikatheengineer <npest17@freeuni.edu.ge>
2021-04-05 23:31:14 +0400

    medzinebaaaa

--- src/components/ImportFileModal.svelte
+++ src/components/ImportFileModal.svelte
@@ -2,7 +2,7 @@
     import { onMount } from 'svelte';
     import WarningModal from '../components/WarningModal.svelte'
 
-    export let show = false, service;
+    export let show = false, service, productsAdded, submitted;
 
     let file;
 
@@ -17,9 +17,12 @@
     });
     
     async function onSubmit() {
+        if(!file) return;
         let res = await service.addProductsFromFile(file);
+        file = null;
+        productsAdded = res.products;
         if (res.status === 200) {
-            submited = true;
+            submitted = true;
             onClose();
         } else {
             warningModalMessage = 'დაფიქსირდა შეცდომა პროდუქტების ჩამატების დროს'
@@ -31,6 +34,10 @@
         show = false;
     }
 
+    function downloadTemplate() {
+        console.log("download template");
+    }
+
     function onChange(event) {
         file = event.srcElement.files[0];
     }
@@ -38,7 +45,7 @@
 </script>
   
 <style>
-    .closeButton {
+    .template-btn {
         background: #3cb5cf !important;
         border-color: #3cb5cf !important;
     }
@@ -51,6 +58,8 @@
         <h5 class="modal-title">აირჩიეთ ფაილი</h5>
         <hr>
         <input type="file" style="border-style: dotted;" on:change={onChange} accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel">
+        <hr>
+        <button class="btn btn-primary template-btn" on:click={downloadTemplate}>Template</button>
         <hr>
         <div>
             <button class="btn btn-primary confirmButton" on:click={onSubmit}>დასტური</button>
